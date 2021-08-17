9aec10a7a2dffdff67dcfa28e6bcb16083e90084
edgarki <edgar@ifly.com.br>
2021-08-17 16:19:46 -0300

    Create Q3.java

---
+++ Q3.java
@@ -0,0 +1,33 @@
+public class Q3
+{
+    
+    private String concat(String[] s){
+        StringBuffer ss = new StringBuffer();
+        for(int i = 0; i < s.length; i++) {
+            ss.append(s[i]);
+        }
+        return ss.toString();
+    }
+    
+    private boolean containSub(String s, String t){
+        return s.indexOf(t) !=-1? true: false;
+    }
+
+    public String concatRemove(String[] s, String[] t, int k)
+    {
+        String S = this.concat(s);
+        String T = this.concat(t);
+        String find = "";
+        for (int i = 0; i < T.length(); i++){
+            if (!this.containSub(S,T.substring(0,i))){
+                find = T.substring(i-1);
+                return S.length()-i-1 <= k? "yes" : "no";
+            }
+        }
+        return "no";
+    }
+    
+    public void use(){
+        System.out.println(this.concatRemove(new String[] {"bla", "bla", "bla", "bla"}, new String[] {"blablabcde"}, 3));
+    }
+}
